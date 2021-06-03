81f79f8c33d348505bc153ad7c101f961453c4ce
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-03 10:14:39 -0500

    added route to notes html in server js

--- server.js
+++ server.js
@@ -5,14 +5,19 @@ const app = express();
 //access to db.json
 const { data } = require('./develop/db/db.json')
 
-app.get('/api/data', (req, res) => {
-    res.json(data);
-});
+// app.get('/api/data', (req, res) => {
+//     res.json(data);
+// });
 
 //to index html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './develop/public/index.html'));
 });
+
+//to notes html
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'))
+})
 
 //server running
 app.listen(3001, () => {
