e3b2bb9e9101e9a82c5b6ebd350484ee51ca6a5d
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-03 11:37:53 -0500

    made route to db.json on server.js

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ const express = require('express');
 const path = require('path')
 const app = express();
 //access to db.json
-const { data } = require('./develop/db/db.json')
+const { notes } = require('./develop/db/db.json')
 
 //require routes
 const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes')
 
 const PORT = process.env.PORT || 3001
-// app.get('/api/data', (req, res) => {
-//     res.json(data);
-// });
 
-//parse incoming string or array data
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+//parse incoming string or array dataS
 app.use(express.urlencoded({ extended: true}));
 //parse incoming JSON data
 app.use(express.json());
