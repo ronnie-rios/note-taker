9eeb424260914012e0d143f972f5d2c104b5c50a
Ronnie Rios <ronniesmail14@gmail.com>
2021-06-04 17:32:52 -0500

    updated port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 //require routes
 const htmlRoutes = require('./routes/htmlRoutes/htmlRoutes')
 const apiRoutes = require('./routes/apiRoutes/notesRoutes')
